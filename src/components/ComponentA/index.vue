5e90844a219e40ef007df8b3ddcd67194025682d
2hangchun <2hangchun@2hangchundeMacBook-Air.local>
2022-04-03 21:21:03 +0800

    forth commit

--- src/components/ComponentA/index.vue
+++ src/components/ComponentA/index.vue
@@ -1,21 +1,31 @@
 <template>
-  <div>
-    <ComponentB
-      class="abc bcd"
-      id="cde"
-      data-title="hello world"
-      value="111"
-      hasaki="yasuo"
-    />
+  <div ref="box" @click="change">
+    {{ value }}
   </div>
 </template>
-
 <script>
-import ComponentB from "./ComponentB";
 export default {
   name: "ComponentA",
-  components: {
-    ComponentB,
+  components: {},
+  data() {
+    return {
+      value: 0,
+    };
+  },
+  methods: {
+    change() {
+      this.value++;
+    },
+  },
+  beforeUpdate() {
+    const box = this.$refs.box;
+    console.log("页面数据", box.innerHTML);
+    console.log("data中数据", this.value);
+  },
+  updated() {
+    const box = this.$refs.box;
+    console.log("页面数据", box.innerHTML);
+    console.log("data中数据", this.value);
   },
 };
 </script>
--- src/main.js
+++ src/main.js
@@ -1,8 +1,7 @@
 import Vue from 'vue'
 import App from './App.vue'
-import ComponentB from './components/ComponentB'
-Vue.component(ComponentB.name, ComponentB)
 Vue.config.productionTip = false
+
 
 new Vue({
   render: h => h(App),
