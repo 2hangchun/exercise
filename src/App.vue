a16ac44ae48752e23118560ed6b448d5c66b8736
2hangchun <2hangchun@2hangchundeMacBook-Air.local>
2022-04-07 20:56:46 +0800

    sixth commit

--- src/App.vue
+++ src/App.vue
@@ -1,16 +1,15 @@
 <template>
   <div id="app">
-    <ComponentA />
+    <h1>hello world</h1>
+    <router-link to="/a">to A</router-link><br />
+    <router-link to="/b">to B</router-link>
+    <router-view></router-view>
   </div>
 </template>
 
 <script>
-import ComponentA from "./components/ComponentA";
 export default {
   name: "App",
-  components: {
-    ComponentA,
-  },
 };
 </script>
 
---
+++ src/components/B/index.vue
@@ -0,0 +1,19 @@
+<template>
+  <h1>我是B组件</h1>
+</template>
+
+<script>
+export default {
+  name: "B",
+  created() {
+    console.log("执行created");
+  },
+  watch: {
+    $route() {
+      console.log("$route变化了");
+    },
+  },
+};
+</script>
+
+<style></style>
